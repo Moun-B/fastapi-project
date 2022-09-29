c3d8fe6012da94ff08f2ed90f2c26f3193d7082e
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-29 19:01:54 +0900

    get individual instance

--- backend/main.py
+++ backend/main.py
@@ -35,5 +35,9 @@ async def create_lead(lead: _schemas.LeadCreate, user: _schemas.User=_fastapi.De
     return await _services.create_lead(user=user, db=db, lead=lead)
 
 @app.get("/api/leads", response_model=List[_schemas.Lead])
-async def get_leads(lead: _schemas.LeadCreate, user: _schemas.User=_fastapi.Depends(_services.get_current_user), db: _orm.Session=_fastapi.Depends(_services.get_db)):
+async def get_leads(user: _schemas.User=_fastapi.Depends(_services.get_current_user), db: _orm.Session=_fastapi.Depends(_services.get_db)):
     return await _services.get_leads(user=user, db=db)
+
+@app.get("/api/leads{lead_id}", status_code=200)
+async def get_lead():
+    pass
