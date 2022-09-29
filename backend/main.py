e38f0bdd65f69723bb803d8e21d3c40a1b495063
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-29 17:36:50 +0900

    finishing auth token

--- backend/main.py
+++ backend/main.py
@@ -17,4 +17,9 @@ async def create_user(user: _schemas.UserCreate, db: _orm.session= _fastapi.Depe
 
 @app.post("/api/token")
 async def generate_token(form_data: _security.OAuth2PasswordRequestForm = _fastapi.Depends(), db: _orm.Session = _fastapi.Depends(_services.get_db)):
-    pass
+    user = await _services.authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise _fastapi.HTTPException(status_code=401, detail="Invalid Credentials")
+
+    return await _services.create_token(user)
