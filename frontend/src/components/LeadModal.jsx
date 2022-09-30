ab46a364302b820221d86eae8ca09f80683433a1
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-30 13:13:40 +0900

    creating the modal component

--- frontend/src/components/LeadModal.jsx
+++ frontend/src/components/LeadModal.jsx
@@ -8,78 +8,125 @@ const LeadModal = ({active, handleModal, token, id, setErrorMessage}) => {
   const [email, setEmail] = useState("");
   const [note, setNote] = useState("");
 
+  const cleanFormData = () => {
+    setFirstName("");
+    setLastName("");
+    setCompany("");
+    setEmail("");
+    setNote("");
+  };
+
+  const handleCreateLead = async (e) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({
+        first_name: firstName,
+        last_name: lastName,
+        company: company,
+        email: email,
+        note: note,
+      }),
+    };
+    const response = await fetch("/api/leads", requestOptions);
+
+    if (!response.ok) {
+      setErrorMessage("Cannot create the Lead");
+    } else {
+      cleanFormData();
+      handleModal();
+    }
+  }
+
   return (
     <div className={`modal ${active && "is-active"}`}>
-      <div className="modal-background" onClick={handleModal}>
-        <div className="modal-card">
-          <header className="modal-card-head has-background-primary-light">
-            <h1 className="modal-card-title">
-              {id ? "Update Lead" : "Create Lead"}
-            </h1>
-          </header>
-          <section className="modal-card-body">
-            <form>
-              <div className="field">
-                <label className="label">First Name</label>
-                <div className="control">
-                  <input
-                    type="text"
-                    placeholder="Enter First Name"
-                    value={firstName} onChange={(e) => setFirstName(e.target.value)}
-                    className="input"
-                    required
-                  />
-                </div>
+      <div className="modal-background" onClick={handleModal}></div>
+      <div className="modal-card">
+        <header className="modal-card-head has-background-primary-light">
+          <h1 className="modal-card-title">
+            {id ? "Update Lead" : "Create Lead"}
+          </h1>
+        </header>
+        <section className="modal-card-body">
+          <form>
+            <div className="field">
+              <label className="label">First Name</label>
+              <div className="control">
+                <input
+                  type="text"
+                  placeholder="Enter First Name"
+                  value={firstName} onChange={(e) => setFirstName(e.target.value)}
+                  className="input"
+                  required
+                />
               </div>
-              <div className="field">
-                <label className="label">Last Name</label>
-                <div className="control">
-                  <input
-                    type="text"
-                    placeholder="Enter Last Name"
-                    value={lastName} onChange={(e) => setLastName(e.target.value)}
-                    className="input"
-                    required
-                  />
-                </div>
+            </div>
+            <div className="field">
+              <label className="label">Last Name</label>
+              <div className="control">
+                <input
+                  type="text"
+                  placeholder="Enter Last Name"
+                  value={lastName} onChange={(e) => setLastName(e.target.value)}
+                  className="input"
+                  required
+                />
               </div>
-              <div className="field">
-                <label className="label">Company</label>
-                <div className="control">
-                  <input
-                    type="text"
-                    placeholder="Enter Company"
-                    value={company} onChange={(e) => setCompany(e.target.value)}
-                    className="input"
-                    required
-                  />
-                </div>
+            </div>
+            <div className="field">
+              <label className="label">Company</label>
+              <div className="control">
+                <input
+                  type="text"
+                  placeholder="Enter Company"
+                  value={company} onChange={(e) => setCompany(e.target.value)}
+                  className="input"
+                  required
+                />
               </div>
-              <div className="field">
-                <label className="label">Email</label>
-                <div className="control">
-                  <input
-                    type="email"
-                    placeholder="Enter Email"
-                    value={email} onChange={(e) => setEmail(e.target.value)}
-                    className="input"
-                  />
-                </div>
+            </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  type="email"
+                  placeholder="Enter Email"
+                  value={email} onChange={(e) => setEmail(e.target.value)}
+                  className="input"
+                />
               </div>
-              <div className="field">
-                <label className="label">Note</label>
-                <div className="control">
-                  <input
-                    type="text"
-                    placeholder="Enter Note"
-                    value={note} onChange={(e) => setNote(e.target.value)}
-                    className="input"
-                  />
-                </div>
+            </div>
+            <div className="field">
+              <label className="label">Note</label>
+              <div className="control">
+                <input
+                  type="text"
+                  placeholder="Enter Note"
+                  value={note} onChange={(e) => setNote(e.target.value)}
+                  className="input"
+                />
               </div>
-            </form>
-          </section>
-        </div>
+            </div>
+          </form>
+        </section>
+        <footer className="modal-card-foot has-background-primary-light">
+          {id ? (
+            <button className="button is-info">
+              Update
+            </button>
+          ) : (
+            <button className="button is-link" onClick={handleCreateLead}>
+              Create
+            </button>
+          )}
+          <button className="button" onClick={handleModal}>
+            Cancel
+          </button>
+        </footer>
       </div>
     </div>
   );
